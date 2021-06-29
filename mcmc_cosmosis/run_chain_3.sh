de88fb83f907f3c8eae23af96df5bac550b9df8c
Marco Gatti <mgatti@ifae.es>
2021-06-29 16:56:39 -0400

    Update run_chain_3.sh

--- mcmc_cosmosis/run_chain_3.sh
+++ mcmc_cosmosis/run_chain_3.sh
@@ -8,8 +8,8 @@
 #SBATCH --constraint=haswell
 #SBATCH --time=48:00:00
 
-export RUNNAME='3tomocross_kEkE_flask_24_1000_reg'
-export KEY_moments='3tomocross_kEkE_flask_24_1000_reg'
+export RUNNAME='3tomocross_kEkE_flask_28_1000_reg'
+export KEY_moments='3tomocross_kEkE_flask_28_1000_reg'
 export DATAFILE=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate.fits
 export DATAFILE_SR=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate_sr.npy
 export SCALEFILE=Y3_params_priors/scales.ini
@@ -33,24 +33,3 @@ srun cosmosis --mpi  Y3_params_priors/params.ini  -p runtime.sampler='polychord'
 
 
 
-
-
-export RUNNAME='2_3tomocross_kEkE_flask_24_1000_reg'
-export KEY_moments='2_3tomocross_kEkE_flask_24_1000_reg'
-export DATAFILE=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate.fits
-export DATAFILE_SR=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate_sr.npy
-export SCALEFILE=Y3_params_priors/scales.ini
-export INCLUDEFILE=Y3_params_priors/params_t17.ini
-export VALUESINCLUDE=Y3_params_priors/values_SR.ini
-export PRIORSINCLUDE=Y3_params_priors/priors.ini
-
-# the following lines you need to change them such that it loads your environment & cosmosis and then move to the mcmc_cosmosis folder. Note that you also need to change the following path to your installation of Moments_analysis.
-export moments_like_path=/global/homes/m/mcraveri/Moments_analysis/mcmc_cosmosis/moment_likelihood.py
-
-cd //global/homes/m/mcraveri/
-source cosmosis/config/setup-cosmosis-nersc 3
-cd /global/homes/m/mcraveri/Moments_analysis/mcmc_cosmosis
-source activate myenv
-
-
-srun cosmosis --mpi  Y3_params_priors/params.ini  -p runtime.sampler='polychord'
