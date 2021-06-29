7ea86dee20a13ac7362b0803d3e314db3cd393eb
Marco Gatti <mgatti@ifae.es>
2021-06-29 16:55:23 -0400

    Update run_chain_23_P.sh

--- mcmc_cosmosis/run_chain_23_P.sh
+++ mcmc_cosmosis/run_chain_23_P.sh
@@ -8,8 +8,8 @@
 #SBATCH --constraint=haswell
 #SBATCH --time=48:00:00
 
-export RUNNAME='2_3tomocross_kEkE_PKD_24_600_reg_200'
-export KEY_moments='2_3tomocross_kEkE_PKD_24_600_reg_200'
+export RUNNAME='2_3tomocross_kEkE_PKD_28_600_reg_200'
+export KEY_moments='2_3tomocross_kEkE_PKD_28_600_reg_200'
 export DATAFILE=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate.fits
 export DATAFILE_SR=data/2pt_NG_final_2ptunblind_02_26_21_wnz_maglim_covupdate_sr.npy
 export SCALEFILE=Y3_params_priors/scales.ini
