7edcc770195ea44f6e23610f8d96792a4c6d824a
Marco Gatti <mgatti29@sas.upenn.edu>
2023-06-05 00:45:37 -0400

    Delete Moments_analysis/.ipynb_checkpoints directory

--- Moments_analysis/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from ._version import __version__, __version_info__
-from .Compute_moments import *
-from .smoothing_utils import *
-from .healpy_utils import *
-from .compute_theory import *
-from .setup_runs_utilities import *
-#import .plotting_utils import *
-#import .moments_analysis_c import *
-
